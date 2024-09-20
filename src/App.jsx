306eff124f1d566cb3345d01bfc281ee3c71a1c0
phoever23 <gaoming93@foxmail.com>
2024-09-20 09:41:02 +0800

    modify app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-// import icons
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'remixicon/fonts/remixicon.css';
-
-// import Bootstrap
+// import Bootstrap and icons
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import './App.css';
 import Header from './components/header/Header';
--- src/components/dashboard/PageContent.jsx
+++ src/components/dashboard/PageContent.jsx
@@ -7,7 +7,7 @@ function PageContent() {
   const [error, setError] = useState(null);
   const [imageUrl, setImageUrl] = useState(null);
 
-  const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
+  const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
   const fetchData = () => {
     setIsLoading(true);
