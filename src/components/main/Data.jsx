c086bf94f59bf7062a27f1ac77518e939f31ff11
phoever23 <gaoming93@foxmail.com>
2024-09-24 10:49:04 +0800

    Create example chart for data tab.

--- src/components/main/Data.jsx
+++ src/components/main/Data.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Column } from '@ant-design/plots';
 import './data.css';
 
 function Data() {
+  const config = {
+    data: {
+      type: 'fetch',
+      value: 'https://render.alipay.com/p/yuyan/180020010001215413/antd-charts/column-column.json',
+    },
+    xField: 'letter',
+    yField: 'frequency',
+    label: {
+      text: (d) => `${(d.frequency * 100).toFixed(1)}%`,
+      textBaseline: 'bottom',
+    },
+    axis: {
+      y: {
+        labelFormatter: '.0%',
+      },
+    },
+    style: {
+      radiusTopLeft: 10,
+      radiusTopRight: 10,
+    },
+  };
+
   return (
-    <div>Data</div>
-  )
+    <div className="data-container">
+      <h2>Data Visualization</h2>
+      <Column {...config} />
+    </div>
+  );
 }
 
-export default Data
+export default Data;
